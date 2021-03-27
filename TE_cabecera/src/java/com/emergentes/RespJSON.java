d2beb542b5d330762c7f152f27ed6c7e61ddf756
Franz-Ruben-Condori-Casas <81276612+Franz-Ruben-Condori-Casas@users.noreply.github.com>
2021-03-26 23:34:14 -0400

    Add files via upload

---
+++ TE_cabecera/src/java/com/emergentes/RespJSON.java
@@ -0,0 +1,30 @@
+package com.emergentes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "RespJSON", urlPatterns = {"/RespJSON"})
+public class RespJSON extends HttpServlet {
+
+@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setHeader("Content-Disposition", "attachment;filename=archivo.csv");
+         PrintWriter out = response.getWriter();
+         try{
+            out.print("{'empleados':{");
+            out.print("{'nombre':'Bruno','apellido':'Diaz'},");
+            out.print("{'nombre':'Juan Jose','apellido':'Paredes'},");
+            out.print("{'nombre':'Dolores','apellido':'Fuertes'}");
+            out.print("]}");
+    }finally{
+            out.close();
+        }
+    }
+}
